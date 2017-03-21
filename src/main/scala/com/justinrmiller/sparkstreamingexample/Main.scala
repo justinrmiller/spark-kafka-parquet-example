2a1f4e0174b9ed924e99aaa83a8eeede525cf8e2
Justin Miller <justinrmiller@gmail.com>
2017-03-21 02:10:19 -0600

    A few updates, successfully consuming from kafka.

--- src/main/scala/com/justinrmiller/sparkstreamingexample/Main.scala
+++ src/main/scala/com/justinrmiller/sparkstreamingexample/Main.scala
@@ -1,14 +1,27 @@
 package com.justinrmiller.sparkstreamingexample
 
-import kafka.serializer.StringDecoder
+import org.apache.kafka.common.serialization.StringDeserializer
+
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.{SparkConf, TaskContext}
 
 object Main {
+  def generateKafkaParams(brokers: String) = {
+    // Create direct kafka stream with brokers and topics]
+    val kafkaParams = Map(
+      "bootstrap.servers" -> brokers,
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test-consumer",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+    kafkaParams
+  }
+
   def main(args: Array[String]) {
     if (args.length < 2) {
       System.err.println(s"""
@@ -24,13 +37,15 @@ object Main {
 
     args.foreach(println(_))
 
+    val topicsSet = topics.split(",").toSet
+
+    val batchDuration = Seconds(30)
+
     // Create context with 2 second batch interval
     val sparkConf = new SparkConf().setAppName("Main").setMaster("local[2]")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val ssc = new StreamingContext(sparkConf, batchDuration)
 
-    // Create direct kafka stream with brokers and topics
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+    val kafkaParams = generateKafkaParams(brokers)
 
     val messages = KafkaUtils.createDirectStream[String, String](
       ssc,
@@ -38,12 +53,17 @@ object Main {
       Subscribe[String, String](topicsSet, kafkaParams)
     )
 
-    // Get the lines, split them into words, count the words and print
-    val lines = messages.map(_.value)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts: DStream[(String, Long)] = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.saveAsTextFiles("/tmp/omg")
-    wordCounts.print
+    messages.foreachRDD { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd.foreachPartition { iter =>
+        val o: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+        println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
+      }
+
+      rdd.foreach(record => println(record.value))
+
+      messages.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
+    }
 
     // Start the computation
     ssc.start()
